6e1426caa0e81b9dcbabb9e8d3ee7fb2c8c6191d
Gl00ria <gl00ria.bbh@gmail.com>
2024-04-09 19:21:18 +0300

    feature: analyze commented line
    parse each line of "code.txt" to detect commented (single, multi) lines

--- src/Main.java
+++ src/Main.java
@@ -12,10 +12,42 @@ public class Main {
             while ((line = br.readLine()) != null) {
                 // Add your function
                 // yourFunction(line, lineNumber);
+                analyze_comment(line, lineNumber, br); // Khalid AbuHamad - 200209977
                 lineNumber++;
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    /*
+     *
+     * Khalid AbuHamad 200209977
+     * ? check line for a (single/multi) line comment
+     *
+     */
+    public static void analyze_comment(String line, int line_num, BufferedReader br) throws IOException {
+        if (line.trim().startsWith("//")) {
+            System.out.println("Line --> " + line_num + ": is a single-line comment");
+        } else if (line.trim().startsWith("/*")) {
+            System.out.println("Line --> " + line_num + ": is a beginning of a multi-line comment");
+            // ? check if the comment ends on the same line */
+            if (line.trim().endsWith("*/")) {
+                System.out.println(
+                        "Line --> " + line_num + ": is the end of multi-line comment stared on the same line :) ehh ");
+            } else {
+                // ? keep it rooling till it hits the end of the multi-line comment
+                int end_line_num = line_num;
+                while ((line = br.readLine()) != null) {
+                    end_line_num++;
+                    if (line.trim().endsWith("*/")) {
+                        System.out.println(
+                                "Line --> " + end_line_num + ": is the end of multi-line comment stared on line "
+                                        + line_num);
+                        break;
+                    }
+                }
+            }
         }
     }
 }
