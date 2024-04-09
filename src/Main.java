cec7ebc2cac519502fa2a9d57a2e88b421757197
ziadwagdy <zwagdy@gmail.com>
2024-04-10 01:32:54 +0300

    [ADD] Main.java: adding isFunction method, sample code

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        String fileName = "src/code.txt";
+        String fileName = "src/testCode.txt";
 
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line;
@@ -12,11 +12,20 @@ public class Main {
             while ((line = br.readLine()) != null) {
                 // Add your function
                 //yourFunction(line, lineNumber);
+                isFunction(line, lineNumber);
                 lineNumber++;
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    
+    public static void isFunction(String line, int lineNumber) {
+        String regex = "^(public|private|protected|static|\\s)*[a-zA-Z0-9<>\\[\\]]*\\s+[a-zA-Z0-9_]+\\s*\\(.*\\)\\s*\\{?\\s*$";
+        if (line.matches(regex)) {
+            System.out.println("Line no: " + lineNumber + " --> " + line.trim() + " is a method declaration");
+        } 
     }
 
 }
