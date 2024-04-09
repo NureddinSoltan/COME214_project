1680d89c26629fca2427325c2c1cdd8850a1d2ba
NureddinSoltan <noorsoltan40@gmail.com>
2024-04-09 16:39:30 +0300

    Add main function + docx file + Update the Readme.md

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String fileName = "src/code.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            int lineNumber = 1;
+            while ((line = br.readLine()) != null) {
+                // Add your function
+                //yourFunction(line, lineNumber);
+                lineNumber++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 }
