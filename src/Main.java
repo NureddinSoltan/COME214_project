0a6c29528f2035b9882a88a9ca296e6e4e9765f1
AbdallahHatemoglu <abdullah.hatemoglu@gmail.com>
2024-04-11 19:04:11 +0300

    Editing the analyze_comment Function + code.txt file

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,13 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        String fileName = "./code.txt";
+        String fileName = "src/code.txt";
 
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line;
             int lineNumber = 1;
             while ((line = br.readLine()) != null) {
-                // Add your function
-                // yourFunction(line, lineNumber);
-                analyze_comment(line, lineNumber, br); // Khalid AbuHamad - 200209977
+                analyze_comment(line, lineNumber/*, br*/);
                 lineNumber++;
             }
         } catch (IOException e) {
@@ -20,34 +18,41 @@ public class Main {
         }
     }
 
-    /*
-     *
-     * Khalid AbuHamad 200209977
-     * ? check line for a (single/multi) line comment
-     *
-     */
-    public static void analyze_comment(String line, int line_num, BufferedReader br) throws IOException {
-        if (line.trim().startsWith("//")) {
-            System.out.println("Line --> " + line_num + ": is a single-line comment");
-        } else if (line.trim().startsWith("/*")) {
-            System.out.println("Line --> " + line_num + ": is a beginning of a multi-line comment");
-            // ? check if the comment ends on the same line */
-            if (line.trim().endsWith("*/")) {
-                System.out.println(
-                        "Line --> " + line_num + ": is the end of multi-line comment stared on the same line :) ehh ");
-            } else {
-                // ? keep it rooling till it hits the end of the multi-line comment
-                int end_line_num = line_num;
-                while ((line = br.readLine()) != null) {
-                    end_line_num++;
-                    if (line.trim().endsWith("*/")) {
-                        System.out.println(
-                                "Line --> " + end_line_num + ": is the end of multi-line comment stared on line "
-                                        + line_num);
-                        break;
-                    }
-                }
+    // This Function analyze a lines of input and checks if they are comments or not
+    public static void analyze_comment(String line, int line_num/*, BufferedReader br*/) /*throws IOException*/ {
+        if (line.startsWith("//") || line.startsWith("/*") && line.endsWith("*/")) {
+            if(line.startsWith("//")) {
+                System.out.println("Line --> " + line_num + ": is a single-line comment");
+            } else{
+                System.out.println("Line --> " + line_num + ": is a multi-line comment");
             }
+        } else if ((line.startsWith("/*") && !(line.endsWith("*/"))) || (!line.startsWith("/*") && (line.endsWith("*/")))){
+            System.out.println("Line --> " + line_num + ": is not a valid multi-line comment");
+        } else if (line.startsWith("/")) {
+            System.out.println("Line --> " + line_num + ": is not a single-line comment");
         }
+        else {
+            System.out.println("Line --> " + line_num + ": is not a comment");
+        }
+//        else if (line.trim().startsWith("/*")) {
+//            System.out.println("Line --> " + line_num + ": is a beginning of a multi-line comment");
+//            // ? check if the comment ends on the same line */
+//            if (line.trim().endsWith("*/")) {
+//                System.out.println(
+//                        "Line --> " + line_num + ": is the end of multi-line comment stared on the same line :) ehh ");
+//            } else {
+//                // ? keep it rooling till it hits the end of the multi-line comment
+//                int end_line_num = line_num;
+//                while ((line = br.readLine()) != null) {
+//                    end_line_num++;
+//                    if (line.trim().endsWith("*/")) {
+//                        System.out.println(
+//                                "Line --> " + end_line_num + ": is the end of multi-line comment stared on line "
+//                                        + line_num);
+//                        break;
+//                    }
+//                }
+//            }
+//        }
     }
 }
