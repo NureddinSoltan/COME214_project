19998e12bc212eb211216c872d1fa365a2480ad2
Noureldien <noureldien>
2024-04-12 02:27:35 +0300

    edit the code logic

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,29 +22,35 @@ public class Main {
     }
 
     public static void checkIdentifiers(String line, int lineNumber) {
-        
-        String[] identifiers = {"if", "for", "while", "switch", "case", "break", "continue",
+        List<String> identifiers = new ArrayList<> (Arrays.asList(
+            "if", "for", "while", "switch", "case", "break", "continue",
                 "return", "try", "catch", "finally", "throw", "throws",
                 "class", "interface", "enum", "public", "private",
                 "protected", "static", "final", "void", "int", "long",
-                "float", "double", "boolean", "char", "String"};
-
-        if (line.trim().startsWith("//") || line.trim().startsWith("/*") || line.trim().endsWith("*/")) {
-            return;
-        }
+                "float", "double", "boolean", "char", "String"
+        ));
 
         boolean foundIdentifier = false;
 
-        for (String identifier : identifiers) {
-            if (line.matches(".*\\b" + identifier + "\\b.*")) {
-                System.out.println("Line no: " + lineNumber + " --> " + identifier + ": is a built-in language construct");
-                foundIdentifier = true;
+        for (String word : line.split(" ")) {
+            if (identifiers.contains(word)){
+                if (line.matches(".*\\b" + word + "\\b.*")) {
+                    System.out.println("Line no: " + lineNumber + " --> " + word + ": is a built-in language construct");
+                    foundIdentifier = true;
+                } 
             }
+            // else {
+            //     System.out.println("Line no: " + lineNumber + " --> : no built-in language construct found");
+            // } 
         }
-    
+        
         if (!foundIdentifier) {
             System.out.println("Line no: " + lineNumber + " --> : no built-in language construct found");
-        }    
+        }
+
+        if (line.trim().startsWith("//") || line.trim().startsWith("/*") || line.trim().endsWith("*/")) {
+            return;
+        }  
     }
 
 }
