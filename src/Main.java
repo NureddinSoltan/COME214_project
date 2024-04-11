fb4b815d5545b3de473c63825d2b2e6dec925669
Noureldien <noureldien>
2024-04-11 12:59:47 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -10,13 +10,36 @@ public class Main {
             String line;
             int lineNumber = 1;
             while ((line = br.readLine()) != null) {
-                // Add your function
-                //yourFunction(line, lineNumber);
+                checkIdentifiers(line, lineNumber);
                 lineNumber++;
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void checkIdentifiers(String line, int lineNumber) {
+        
+        String[] identifiers = {"if", "for", "while", "switch", "case", "break", "continue",
+                "return", "try", "catch", "finally", "throw", "throws",
+                "class", "interface", "enum", "public", "private",
+                "protected", "static", "final", "void", "int", "long",
+                "float", "double", "boolean", "char", "String"};
+
+        for (String identifier : identifiers) {
+            if (line.matches(".*\\b" + identifier + "\\b.*")) {
+                System.out.println("Line no: " + lineNumber + " --> " + identifier + " is a built-in language construct");
+                break;
+            }
+        }
+                /* String pattern = "\\b(?:" + String.join("|", identifiers) + ")\\b";
+        Pattern r = Pattern.compile(pattern);
+
+        Matcher m = r.matcher(line);
+        while (m.find()) {
+            System.out.println("Line no: " + lineNumber + " --> " + m.group() + " is a built-in language construct");
+        }*/
+
     }
 
 }
