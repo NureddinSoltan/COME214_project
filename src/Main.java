0a73c7cd69fa8e1cf4aea6681283d0cd941b3da7
Noureldien <noureldien>
2024-04-12 19:03:45 +0300

    rebuild the file

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
 
 public class Main {
     public static void main(String[] args) {
@@ -16,14 +13,7 @@ public class Main {
             String line;
             int lineNumber = 1;
             while ((line = br.readLine()) != null) {
-                String loopType = getLoopType(line);
-                if (loopType.equals("while")) {
-                    System.out.println("Line " + lineNumber + ": Belongs to While Loop");
-                } else if (loopType.equals("for")) {
-                    System.out.println("Line " + lineNumber + ": Belongs to For Loop");
-                } else {
-                    System.out.println("Line " + lineNumber + ": Does not belong to either For or While Loop");
-                }
+                loopType(line, lineNumber);
                 lineNumber++;
             }
         } catch (IOException e) {
@@ -31,22 +21,7 @@ public class Main {
         }
     }
 
-    public static String getLoopType(String line) {
-        String forLoopPattern = "^\\s*for\\s*\\(\\s*(?:int\\s+)?\\w+\\s*=\\s*\\w+\\s*;\\s*\\w+\\s*[<>]=?\\s*\\w+\\s*;\\s*\\w+\\s*=\\s*\\w+(?:\\s*\\+\\s*\\d+)?\\s*\\)\\s*(?:\\{)?\\s*$";
-        String whileLoopPattern = "^\\s*while\\s*\\(\\s*\\w+\\s*[<>!=]=?\\s*\\w+\\s*\\)\\s*.*$";
-
-        Pattern forPattern = Pattern.compile(forLoopPattern);
-        Matcher forMatcher = forPattern.matcher(line);
-        if (forMatcher.matches()) {
-            return "for";
-        }
-
-        Pattern whilePattern = Pattern.compile(whileLoopPattern);
-        Matcher whileMatcher = whilePattern.matcher(line);
-        if (whileMatcher.matches()) {
-            return "while";
-        }
+    public static void loopType(String line, int lineNumber) {
         
-        return "none";
     }
 }
