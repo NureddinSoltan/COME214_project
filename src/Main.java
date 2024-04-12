427bb51e68bbd9b6d740db69c92e9c1e1770d0b5
Seif Elesllam <seifsaif151@gmail.com>
2024-04-12 21:02:27 +0300

    finished while ex doc

--- src/Main.java
+++ src/Main.java
@@ -9,21 +9,14 @@ import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
-        // String fileName = "code.txt";
-        Path path = Paths.get("src","code.txt");
+        String fileName = "code.txt";
+        // Path path = Paths.get("src","code.txt");
 
-        try (BufferedReader br = new BufferedReader(new FileReader(path.toString()))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line;
             int lineNumber = 1;
             while ((line = br.readLine()) != null) {
-                String loopType = getLoopType(line);
-                if (loopType.equals("while")) {
-                    System.out.println("Line " + lineNumber + ": Belongs to While Loop");
-                } else if (loopType.equals("for")) {
-                    System.out.println("Line " + lineNumber + ": Belongs to For Loop");
-                } else {
-                    System.out.println("Line " + lineNumber + ": Does not belong to either For or While Loop");
-                }
+                PrintLoop(line, lineNumber);
                 lineNumber++;
             }
         } catch (IOException e) {
@@ -31,10 +24,22 @@ public class Main {
         }
     }
 
-    public static String getLoopType(String line) {
-        String forLoopPattern = "^\\s*for\\s*\\(\\s*(?:int\\s+)?\\w+\\s*=\\s*\\w+\\s*;\\s*\\w+\\s*[<>]=?\\s*\\w+\\s*;\\s*\\w+\\s*=\\s*\\w+(?:\\s*\\+\\s*\\d+)?\\s*\\)\\s*(?:\\{)?\\s*$";
-        String whileLoopPattern = "^\\s*while\\s*\\(\\s*\\w+\\s*[<>!=]=?\\s*\\w+\\s*\\)\\s*.*$";
 
+    public static void PrintLoop(String line,int lineNumber){
+        String loopType = getLoopType(line);
+        if (loopType.equals("while")) {
+            System.out.println("Line " + lineNumber + ": Belongs to While Loop");
+        } else if (loopType.equals("for")) {
+            System.out.println("Line " + lineNumber + ": Belongs to For Loop");
+        } else {
+            System.out.println("Line " + lineNumber + ": Does not belong to either For or While Loop");
+        }
+    }
+
+
+    public static String getLoopType(String line) {
+        String whileLoopPattern = "^\\s*while\\s*\\(\\s*\\w+\\s*[<>!=]=?\\s*\\w+\\s*\\)\\s*.*$";
+        String forLoopPattern = "^\\s*for\\s*\\(\\s*(?:(?:int|double|String|char|boolean|long|float|short|byte)\\s+)?\\w+\\s*=\\s*\\w+\\s*;\\s*\\w+\\s*[<>]=?\\s*\\w+\\s*;\\s*\\w+\\s*=\\s*\\w+(?:\\s*[+-/]\\s*\\d+)?\\s*\\)\\s*(?:\\{)?\\s*$";
         Pattern forPattern = Pattern.compile(forLoopPattern);
         Matcher forMatcher = forPattern.matcher(line);
         if (forMatcher.matches()) {
@@ -50,3 +55,5 @@ public class Main {
         return "none";
     }
 }
+
+
